14468ac5f8aad2fde3b001d46d0708fb635709e0
Ryan Douglas <ryan@luckydogdigital.com>
2016-03-22 11:50:36 -0600

    got the basics written

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var http = require('http');
-var Socket = require('socket.io');
+var socket = require('socket.io');
 var port = 3000;
 
 var app = express();
 var server = http.Server(app);
+var io = socket(server);
 
+function socketTester() {
+   return Math.floor(Math.random() * 20);
+}
+
+var tapOpen = false;
+
+function tapTimeout() {
+  
+}
+
+io.on('connection', function(socket){
+  setInterval(function(){
+    var pourVolume = socketTester();
+    socket.emit('pourVolumeData', {
+      body: pourVolume
+    })
+  })
+})
+
+setTimeout
 
 
 server.listen(port, function(){
